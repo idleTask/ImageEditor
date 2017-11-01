22abccd9d2f839c53f2039d4f5097a203613a123
janman22 <jannick.apitz@gmail.com>
2017-11-01 11:09:57 +0100

    Add menu bar

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -4,15 +4,34 @@ import java.awt.*;
 import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
-public class ImageEditorFrame extends JFrame{
- public ImageEditorFrame(){
- setDefaultCloseOperation(EXIT_ON_CLOSE);
- setSize(400, 300);
- setVisible(true);
- }
- public static void main (String[] args){
-	 ImageEditorFrame frame = new ImageEditorFrame();
- }
+
+public class ImageEditorFrame extends JFrame {
+    public ImageEditorFrame() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(400, 300);
+        createMenuBar();
+        setVisible(true);
+    }
+
+    public static void main (String[] args) {
+        ImageEditorFrame frame = new ImageEditorFrame();
+    }
+
+    private void createMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        JMenu menuFile = new JMenu("File");
+        menuBar.add(menuFile);
+        JMenuItem menuItemOpen = new JMenuItem("Open");
+        menuFile.add(menuItemOpen);
+        menuItemOpen.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               onOpen();
+            }
+        });
+    }
+
+    private void onOpen() {
+        JOptionPane.showMessageDialog(this, "Open Selected");
+    }
 }
-
-
